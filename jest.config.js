6dd3f8b35103f2cdb063770a40cc752e2b08d579
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2020-11-06 19:45:56 -0500

    add helper functions

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/tests/jest/jest.setup.ts'],
 };
--- src/index.ts
+++ src/index.ts
@@ -39,3 +39,38 @@ export function fetcher<T = unknown>(opts: ClientConfiguration = {}): Fetcher<T>
     });
   };
 }
+
+function isString(val: any): val is string {
+  return typeof val === 'string';
+}
+
+function isBlob(value: any): value is Blob {
+  return value instanceof Blob;
+}
+
+export function getRequestBody(body: any): BodyInit | undefined {
+  if (body === undefined) {
+    return undefined;
+  }
+
+  if (isString(body)) {
+    return body;
+  }
+
+  if (isBlob(body)) {
+    return body;
+  }
+
+  return JSON.stringify(body);
+}
+
+export function getResponseBody(response: Response) {
+  const contentType = response.headers.get('Content-Type') ?? '';
+  const isJSON = contentType.toLowerCase().startsWith('application/json');
+
+  if (isJSON) {
+    return response.json();
+  }
+
+  return response.text();
+}
